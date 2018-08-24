1e79630287372c1be2f30b9ce49678f4a5f8e3ad
kylemchown <kyle.m.chown@gmail.com>
2018-08-24 11:49:58 +0100

    Added flee option

--- Java Assessment/src/assessment/Monster.java
+++ Java Assessment/src/assessment/Monster.java
@@ -12,9 +12,10 @@ public class Monster extends Fighter {
 	public void fight(Player player) {
 		System.out.println("A Monster Appears");
 		int turn = 0;
+		int flee = 0;
 		while (player.getHp() > 0 && this.getHp() > 0) {
 			System.out.println("Select commands by using the assosciated number");
-			System.out.println("Choose a command: (1) Fight / (2) Guard");
+			System.out.println("Choose a command: (1) Fight / (2) Guard / (3) Flee");
 			String action = s.nextLine();
 			int monatk = ThreadLocalRandom.current().nextInt((int)(this.getAtk() - ((int) this.getAtk() * 0.25)), (int) (this.getAtk() + ((int) this.getAtk() * 0.25)));
 			int plaatk = ThreadLocalRandom.current().nextInt((int)(player.getAtk() - ((int) player.getAtk() * 0.25)), (int) (player.getAtk() + ((int) player.getAtk() * 0.25)));
@@ -32,6 +33,10 @@ public class Monster extends Fighter {
 				System.out.println("You defend yourself");
 				monatk = monatk / 2;
 				break;
+			case"3":
+				System.out.println("You attempt to flee");
+				flee = ThreadLocalRandom.current().nextInt(1, 11);
+				break;
 			default:
 				System.out.println("Please enter a valid command");
 				continue;
@@ -43,12 +48,16 @@ public class Monster extends Fighter {
 			if (turn % 3 == 2) {
 				System.out.println("The monster is gearing up for a strong attack");
 			}
+			if (flee > 5) {
+				System.out.println("You successfully flee");
+				this.setHp(0);
+			}
 		}
 		if (this.getHp() <= 0) {
-			System.out.println("You beat the monster!");
+			System.out.println("You beat the monster! (or ran away, which is kind of like winning since it means you didn't die)");
 		}
 		else {
-			System.out.println("You died. Maybe try harder next time.");
+			System.out.println("You died :( . Maybe try again?");
 			System.exit(0);
 		}
 	}
